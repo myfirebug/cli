e7ba717901cb414aa0e4b83b77082225b40f7969
apple <ty-apple@mail.idcduty.com>
2023-07-25 23:11:57 +0800

    fix: 拆分打包环境

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,108 +1,11 @@
-const ESLintPlugin = require("eslint-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackBar = require("webpackbar");
-const path = require("path");
-const { tools } = require("./utils");
-const { getStyleLoader } = tools;
+const merge = require("webpack-merge");
+const portfinder = require("portfinder");
+const baseConfig = require("./webpack.base.js");
 
-module.exports = {
-  // 入口相对路径
-  entry: "./src/main.js",
-  // 输出
-  output: {
-    // 文件的输出路径，绝对路由
-    // __dirname nodejs的变量，代表当前文件的文件夹目录
-    path: undefined,
-    // 入口打包输出的文件名
-    filename: "static/js/[name].js",
-    // 图片，字体，通过type：asset处理的资源全名方式
-    assetModuleFilename: "static/media/[hash:10][ext][query]",
-    // 打包前将path的整个目录内容清空，在进行打包
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        oneOf: [
-          // 处理css
-          {
-            test: /\.css$/i,
-            // use 执行顺序：从右到左，从下到上
-            use: getStyleLoader(),
-          },
-          {
-            test: /\.s[ac]ss$/i,
-            use: getStyleLoader("scss-loader"),
-          },
-          // 处理图片
-          {
-            test: /\.(png|jpe?g|gif|webp|svg)$/,
-            type: "asset",
-            parser: {
-              dataUrlCondition: {
-                maxSize: 10 * 1024, // 小于10kb转成base64
-              },
-            },
-          },
-          {
-            test: /\.(ttf|woff2?|mp3|mp4|avi)$/,
-            type: "asset/resource",
-          },
-          // 处理js
-          {
-            test: /\.(js|ts)x?$/,
-            exclude: /node_modules/, // 排除node_modules中的js文件
-            use: [
-              {
-                loader: "babel-loader",
-                options: {
-                  cacheDirectory: true, // 开启babel缓存
-                  cacheCompression: false, // 关闭缓存文件压缩
-                },
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new ESLintPlugin({
-      exclude: "node_modules",
-      context: path.resolve(__dirname, "../src"),
-      cache: true,
-      cacheLocation: path.resolve(
-        __dirname,
-        "../node_modules/.cache/.eslintcache"
-      ),
-    }),
-    new HtmlWebpackPlugin({
-      // 模板：以public/index.html文件创建新的html文件
-      // 新的文件特点：结构和原来一致，自动引入打包出的资源
-      template: path.resolve(__dirname, "../public/index.html"),
-      minify: {
-        // 压缩 HTML 文件
-        removeComments: true, // 移除 HTML 中的注释
-        collapseWhitespace: true, // 删除空白符与换行符
-        minifyCSS: true, // 压缩内联 css
-      },
-      templateParameters: {
-        PUBLIC_URL: "/",
-      },
-    }),
-    new WebpackBar(),
-  ],
+const devConfig = merge.merge(baseConfig, {
   // 模式
   mode: "development",
   devtool: "cheap-module-source-map",
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
-    runtimeChunk: {
-      name: (entrypoint) => `runtime~${entrypoint.name}`,
-    },
-  },
   // webpack 解析模块加载选项
   resolve: {
     // 自动补全文件扩展名
@@ -111,7 +14,34 @@ module.exports = {
   devServer: {
     host: "localhost", // 启动服务域名
     open: true, // 启动服务端口
-    port: 9000, // 是否自动打开浏览器
+    port: 3000, // 是否自动打开浏览器
     hot: true,
+    historyApiFallback: true,
+    onListening: function (devServer) {
+      if (!devServer) {
+        throw new Error("webpack-dev-server is not defined");
+      }
+      const port = devServer.server.address().port;
+      console.log("您当前运行的端口是:", port);
+    },
   },
-};
+});
+
+module.exports = new Promise((resolve, reject) => {
+  // Auto find port
+  portfinder.getPort(
+    {
+      port: 3000,
+      stopPort: 8000,
+    },
+    (err, port) => {
+      if (err) {
+        reject(err);
+        throw new Error("can not fond available port! try again...");
+      }
+      // When the port is occupied, reset the port of evn and devserver
+      devConfig.devServer.port = port;
+      resolve(devConfig);
+    }
+  );
+});
